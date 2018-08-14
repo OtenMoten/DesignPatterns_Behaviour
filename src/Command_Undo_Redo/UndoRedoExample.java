725076a5de27f7854a58286eb35bd554e5666119
Kevin <Kevin@Panzer>
2018-08-14 16:15:31 +0200

    Final Version.

---
+++ src/Command_Undo_Redo/UndoRedoExample.java
@@ -0,0 +1,94 @@
+/**
+ * In this package you will learn how to manage a undo- and redo-function related
+ * to a user's input on a keyboard.
+ *
+ * Enjoy the course and feel free to contribute.
+ */
+package Command_Undo_Redo;
+
+/**
+ * @author Kevin Ossenbrück
+ * @date August 2018
+ * @github https://github.com/OtenMoten
+ */
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.undo.UndoManager;
+
+public class UndoRedoExample {
+
+    public static void main(String[] args) {
+
+        //  Create a object of the "UndoManager"-class.
+        //  We use the manager from the Swing-library (javax.swing.undo).
+        UndoManager undoRedomanager = new UndoManager();
+
+        //  Create a frame and
+        JFrame mainFrame = new JFrame();
+        //  a textarea and 
+        JTextArea textarea = new JTextArea(10, 20);
+        //  two buttons.
+        JButton btnUndo = new JButton("Undo");
+        JButton btnRedo = new JButton("Redo");
+
+        //  Define the GUI elements' attributes.
+        mainFrame.setLocation(300, 300);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        textarea.setText("");
+        textarea.setText("Type here ...");
+        textarea.setFont(new Font("Helvetica", 0, 40));
+        //  Add a listener in the textarea.
+        //  The keyword here is the "UndoableEditListener".
+        //  Because the textarea is supporting a function where we can add
+        //  a listener like our undomanager-object.
+        //  The undomanager-object is type of the "UndoManger"-class which
+        //  implements the "UndoableEditListener"-interface.
+        textarea.getDocument().addUndoableEditListener(undoRedomanager);
+        //  Set up the maximal count of the history.
+        undoRedomanager.setLimit(240);
+
+        //  Create a action-listener for the undo-button.
+        ActionListener undo = (ActionEvent actionEvent) -> {
+            //  Check if history is not empty.
+            if (undoRedomanager.canUndo()) {
+                //  Undo last action.
+                undoRedomanager.undo();
+                System.out.println("The button: '" + actionEvent.getActionCommand() + "' has been clicked.");
+            }
+            //  Switch the cursor back in the textarea.
+            textarea.requestFocus();
+        };
+        //  Create a action-listener for the redo-button.
+        ActionListener redo = (ActionEvent actionEvent) -> {
+            //  Check if history is not empty.
+            if (undoRedomanager.canRedo()) {
+                //  Redo last action.
+                undoRedomanager.redo();
+                System.out.println("The button: '" + actionEvent.getActionCommand() + "' has been clicked.");
+            }
+            //  Switch the cursor back in the textarea.
+            textarea.requestFocus();
+        };
+
+        //  Install the listeners on the buttons and
+        btnUndo.addActionListener(undo);
+        btnRedo.addActionListener(redo);
+        //  add the textarea to the frame.
+        mainFrame.add(textarea);
+        //  At least add the buttons to the frame.
+        mainFrame.add(btnUndo, BorderLayout.WEST);
+        mainFrame.add(btnRedo, BorderLayout.EAST);
+
+        //  Start the program.
+        mainFrame.pack();
+        mainFrame.setVisible(true);
+
+    }
+
+}
--- src/Command_Undo_Textinput/UndoExample.java
+++ src/Command_Undo_Textinput/UndoExample.java
@@ -1,18 +1,19 @@
 /**
- * Author:  Kevin Ossenbrück
- * Date:    August 2018
- *
  * In this package you will learn how to manage a undo-function related to
- * a user-input on a keyboard.
+ * a user's input on a keyboard.
  *
- * Enjoy and feel free to contribute.
+ * Enjoy the course and feel free to contribute.
  */
-package Command_Textinput_Undo;
+package Command_Undo_Textinput;
 
+/**
+ * @author Kevin Ossenbrück
+ * @date August 2018
+ * @github https://github.com/OtenMoten
+ */
 import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -24,7 +25,7 @@ public class UndoExample {
     public static void main(String[] args) {
 
         //  Create a object of the "UndoManager"-class.
-        //  We use the manager from the Swing-library (javax.swing.undo.UndoManager)-
+        //  We use the manager from the Swing-library (javax.swing.undo.UndoManager).
         UndoManager undomanager = new UndoManager();
 
         //  Create a frame and
@@ -32,32 +33,13 @@ public class UndoExample {
         //  a textarea and
         final JTextArea textarea = new JTextArea(5, 10);
         //  a button.
-        JButton undoB = new JButton("Undo");
+        JButton btnUndo = new JButton("Undo");
 
-        //  Define the frame's attributes.
+        //  Define the GUI elements' attributes.
         mainFrame.setLocation(300, 300);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //  Set up the initial text of the textarea.
-        textarea.setText("X");
-        //  Define the textarea's attributes, like the style-type and size.
+        textarea.setText("Type here ...");
         textarea.setFont(new Font("Helvetica", 0, 40));
-        //  Install a action-listener on the button.
-        undoB.addActionListener((ActionEvent e) -> {
-            //  Check if history is not empty.
-            if (undomanager.canUndo()) {
-                //  Undo last action.
-                undomanager.undo();
-                System.out.println("The button: '" + e.getActionCommand() + "' has pushed.");
-            }
-            //  Switch the cursor back in the textarea.
-            textarea.requestFocus();
-        });
-
-        //  Put the textarea and
-        mainFrame.add(textarea);
-        //  the button in the frame.
-        mainFrame.add(undoB, BorderLayout.SOUTH);
-
         //  Add a listener in the textarea.
         //  The keyword here is the "UndoableEditListener".
         //  Because the textarea is supporting a function where we can add
@@ -67,6 +49,23 @@ public class UndoExample {
         textarea.getDocument().addUndoableEditListener(undomanager);
         //  Set up the maximal count of the history.
         undomanager.setLimit(120);
+        
+        //  Install a action-listener on the button.
+        btnUndo.addActionListener((ActionEvent actionEvent) -> {
+            //  Check if history is not empty.
+            if (undomanager.canUndo()) {
+                //  Undo last action.
+                undomanager.undo();
+                System.out.println("The button: '" + actionEvent.getActionCommand() + "' has been clicked.");
+            }
+            //  Switch the cursor back in the textarea.
+            textarea.requestFocus();
+        });
+
+        //  Put the textarea and
+        mainFrame.add(textarea);
+        //  the button into the frame.
+        mainFrame.add(btnUndo, BorderLayout.SOUTH);
 
         //  Start the program. 
         mainFrame.pack();
